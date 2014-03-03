8ebd6e9dab5514b3f191dc9bb8dd3febe3d3e41f
John Morton <john@johnfmorton.com>
2014-03-03 14:12:53 -0500

    Added Dashboard widget
    - still needs to use the logged in user instead of hard coded "1"
    - needs an admin panel for widget to customize functionality

---
+++ craft/plugins/picpuller/widgets/PicPuller_LatestWidget.php
@@ -0,0 +1,38 @@
+<?php
+namespace Craft;
+
+defined('CRAFT_PLUGINS_PATH')      || define('CRAFT_PLUGINS_PATH',      CRAFT_BASE_PATH.'plugins/');
+
+require_once(CRAFT_PLUGINS_PATH.'picpuller/lib/FirePHPCore/fb.php');
+/*
+
+Digging around? Enable FirePHP debugging by changin "devMode" to true in your config file, or, FB::setEnabled(true);
+You'll need to use FirePHP for Firefox or FirePHP4Chrome and look at your console in your web browser
+
+*/
+
+\FB::setEnabled(craft()->config->get('devMode'));
+
+// Examples:
+// \FB::log('Log message', 'Label');
+// \FB::info('Info message', 'Label');
+// \FB::warn('Warn message', 'Label');
+// \FB::error('Error message', 'Label');
+
+class PicPuller_LatestWidget extends BaseWidget
+{
+    public function getName()
+    {
+        return Craft::t('Latest Instagram Image');
+    }
+
+    public function getBodyHtml()
+    {
+        $media_recent = craft()->picPuller_feedReader->media_recent(array('user_id' => 1, 'limit' => 1));
+         \FB::log($media_recent, '$media_recent');
+        $renderedTemplate = craft()->templates->render('picpuller/_widgets/latest',  array( 'media_recent' => $media_recent) );
+        
+        //$renderedTemplate = craft()->templates->render('picpuller/_widgets/latest');
+        return $renderedTemplate;
+    }
+}
